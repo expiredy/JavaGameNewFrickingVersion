56d76927cb1cbfd617cdf16303d5ee7e8f0bb41a
expiredy101 <yartaoley2005@mail.ru>
2021-05-28 11:23:51 +0300

    Adding some old sprites (Why not) and still have no idea how i can finish this game in 3 hours))))

--- core/src/com/mygdx/balls/constants/ViewConstants.java
+++ core/src/com/mygdx/balls/constants/ViewConstants.java
@@ -1,6 +1,8 @@
 package com.mygdx.balls.constants;
 
+import com.badlogic.gdx.Gdx;
+
 public class ViewConstants {
-    public final static Integer VIRTUAL_WIDTH = 1080;
-    public final static Integer VIRTUAL_HEIGHT = 1920;
+    public final static Integer VIRTUAL_WIDTH = Gdx.graphics.getWidth();
+    public final static Integer VIRTUAL_HEIGHT = Gdx.graphics.getHeight();
 }
